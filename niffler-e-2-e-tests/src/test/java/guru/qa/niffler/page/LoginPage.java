8217147125a2ab77577d73128bdbd88955b1f095
ns <Shpakov.NN@ya.ru>
2024-09-23 23:59:11 +0300

    Подлил изменения из основного репозитория

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
@@ -2,6 +2,7 @@ package guru.qa.niffler.page;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
@@ -10,6 +11,13 @@ public class LoginPage {
     private final SelenideElement submitButton = $("button[type='submit']");
     private final SelenideElement registerButton = $(".form__register");
     private final SelenideElement title = $(".form__error");
+    private final SelenideElement errorContainer = $(".form__error");
+
+
+    public RegisterPage doRegister() {
+        registerButton.click();
+        return new RegisterPage();
+    }
 
     public MainPage login(String username, String password) {
         usernameInput.setValue(username);
@@ -21,6 +29,16 @@ public class LoginPage {
     public RegisterPage clickRegisterButton() {
         registerButton.click();
         return new RegisterPage();
+    }
+
+    public MainPage successLogin(String username, String password) {
+        login(username, password);
+        return new MainPage();
+    }
+
+    public LoginPage checkError(String error) {
+        errorContainer.shouldHave(text(error));
+        return this;
     }
 
     public void shouldBeDisplayedErrorForm(String value) {
